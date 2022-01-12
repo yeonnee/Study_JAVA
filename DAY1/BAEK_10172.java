6882048e5c7cd5c48d7a440fbac383e5636964ac
yeonnee <wjy0429won@gmail.com>
2022-01-12 16:29:11 +0900

    WEEK1

--- DAY1/BAEK_10172.java
+++ DAY1/BAEK_10172.java
@@ -2,7 +2,7 @@ public class BAEK_10172 {
     public static void main(String[] args){
 
         StringBuffer sb = new StringBuffer();
-        sb.append("|\\_/|\n");
+//sb.append("|\\_/|\n");
         sb.append("|q p|   /}\n");
         sb.append("( 0 )\"\"\"\\\n");
         sb.append("|\"^\"`    |\n");
