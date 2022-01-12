ff4c2420479b39f0bf816efc3d165ce03aa824ff
yeonnee <wjy0429won@gmail.com>
2022-01-12 15:42:09 +0900

    WEEK1

---
+++ DAY1/BAEK_10171.java
@@ -0,0 +1,12 @@
+public class BAEK_10171{
+    public static void main(String[] args){
+
+        StringBuffer sb = new StringBuffer();
+        sb.append("\\    /\\\n");
+        sb.append(")  ( ')\n");
+        sb.append("(  /  )\n");
+        sb.append("\\(__)|\n");
+
+        System.out.println(sb);
+    }
+}
