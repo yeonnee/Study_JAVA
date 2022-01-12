403def09fdd413ef631d2aa2b7ffe7fca0ffb607
yeonnee <wjy0429won@gmail.com>
2022-01-12 16:17:49 +0900

    WEEK1

---
+++ DAY1/BAEK_1000.java
@@ -0,0 +1,14 @@
+import java.util.*;      //import java.util.Scanner
+public class BAEK_1000 {
+    public static void main(String[] args){
+        
+        Scanner scanner = new Scanner(System.in);
+
+        int a, b;
+        a = scanner.nextInt();
+        b = scanner.nextInt();
+        System.out.println(a+b);
+
+        scanner.close();
+    }  
+}
---
+++ DAY1/BAEK_10172.java
@@ -0,0 +1,13 @@
+public class BAEK_10172 {
+    public static void main(String[] args){
+
+        StringBuffer sb = new StringBuffer();
+        sb.append("|\\_/|\n");
+        sb.append("|q p|   /}\n");
+        sb.append("( 0 )\"\"\"\\\n");
+        sb.append("|\"^\"`    |\n");
+        sb.append("||_/=\\\\__|\n");
+
+        System.out.println(sb);
+    }
+}
