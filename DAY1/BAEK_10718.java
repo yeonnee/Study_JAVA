6df92a68cc300e42dbd88281b60df6b30f319c1d
yeonnee <wjy0429won@gmail.com>
2022-01-12 15:22:05 +0900

    WEEK1

---
+++ DAY1/BAEK_10718.java
@@ -0,0 +1,7 @@
+public class BAEK_10718{    
+    public static void main(String[] args){
+        System.out.println("강한친구 대한육군");
+        System.out.println("강한친구 대한육군");
+    }
+
+}
--- DAY1/BAEK_2557.java
+++ DAY1/BAEK_2557.java
@@ -1,4 +1,4 @@
-public class hello{    
+public class BAEK_2557{    
     public static void main(String[] args){
         System.out.println("Hello World");
     }
