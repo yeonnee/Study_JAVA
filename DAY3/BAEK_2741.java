a93f2f8df20d415549f583c8318bd1b13ab8e1e2
yeonnee <wjy0429won@gmail.com>
2022-01-17 17:45:49 +0900

    WEEK1

---
+++ DAY3/BAEK_2741.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class BAEK_2741{
+    public static void main(String[] args){
+        
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+
+        for(int i =1; i<=num; i++){
+            System.out.println(i);
+        }
+
+        sc.close();
+    }
+}
