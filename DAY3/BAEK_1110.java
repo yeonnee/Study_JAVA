0e4447476c8e9f7f6140a6779a981e4deb62b8b4
yeonnee <wjy0429won@gmail.com>
2022-01-20 17:19:16 +0900

    WEEK2

---
+++ DAY3/BAEK_1110.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BAEK_1110{
+    public static void main(String[] args) throws IOException{
+        
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int count = 0;
+        int copy = N;
+
+        do{
+            N = (((N%10)*10) + (((N/10)+(N%10))%10));
+            count++;
+        }while(copy != N);
+
+        System.out.println(count);
+
+        br.close();
+    }
+}
